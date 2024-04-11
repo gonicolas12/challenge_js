09b19c5521d8c61ab818b9a426fe447c2229b0a2
Shems <shemsedine.malagouen@ynov.com>
2024-04-11 16:03:54 +0200

    vs snake fonctionel

--- web/assets/js/snake.js
+++ web/assets/js/snake.js
@@ -1,83 +1,92 @@
-const canvas = document.getElementById('snakeGame');
-const ctx = canvas.getContext('2d');
+let canvas = document.getElementById('snakeGame');
+let ctx = canvas.getContext('2d');
 const box = 20;
 let snake = [];
-snake[0] = { x: 9 * box, y: 10 * box };
-let food = { x: Math.floor(Math.random() * 19 + 1) * box, y: Math.floor(Math.random() * 19 + 1) * box };
 let score = 0;
 let d;
-
-let snakeHeadImg = new Image();
-snakeHeadImg.onload = function() { draw(); }
-snakeHeadImg.src = '../img/CorpsSerpent.png';
-let snakeBodyImg = new Image();
-snakeBodyImg.src = '../img/CorpsSerpent.png';
-let fruitImg = new Image();
-fruitImg.src = '../img/PommeSnake.png';
-
-document.addEventListener("keydown", direction);
-function direction(event) {
-    if (event.keyCode == 37 && d != "RIGHT") d = "LEFT";
-    else if (event.keyCode == 38 && d != "DOWN") d = "UP";
-    else if (event.keyCode == 39 && d != "LEFT") d = "RIGHT";
-    else if (event.keyCode == 40 && d != "UP") d = "DOWN";
+let game;
+let food;
+let appleImg = new Image();
+appleImg.src = '../assets/img/PommeSnake.png';
+let snakeImg = new Image();
+snakeImg.src = '../assets/img/TeteSerpent.png';
+function createFood() {
+  food = { x: Math.floor(Math.random() * (canvas.width / box - 1) + 1) * box, y: Math.floor(Math.random() * (canvas.height / box - 1) + 1) * box };
 }
 
 function collision(head, array) {
-    for (let i = 0; i < array.length; i++) {
-        if (head.x == array[i].x && head.y == array[i].y) return true;
+  for (let i = 0; i < array.length; i++) {
+    if (head.x == array[i].x && head.y == array[i].y) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#282c34";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < snake.length; i++) {
+    ctx.drawImage(snakeImg, snake[i].x, snake[i].y, box, box);
+  }
 
-    for (let i = 0; i < snake.length; i++) {
-        let img = (i == 0) ? snakeHeadImg : snakeBodyImg;
-        ctx.drawImage(img, snake[i].x, snake[i].y, box, box);
-    }
+  ctx.drawImage(appleImg, food.x, food.y, box, box);
 
-    ctx.drawImage(fruitImg, food.x, food.y, box, box);
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
 
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
+  if (d == "LEFT") snakeX -= box;
+  if (d == "UP") snakeY -= box;
+  if (d == "RIGHT") snakeX += box;
+  if (d == "DOWN") snakeY += box;
 
-    if (d == "LEFT") snakeX -= box;
-    if (d == "UP") snakeY -= box;
-    if (d == "RIGHT") snakeX += box;
-    if (d == "DOWN") snakeY += box;
+  if (snakeX == food.x && snakeY == food.y) {
+    score++;
+    createFood();
+  } else {
+    snake.pop();
+  }
 
-    if (snakeX == food.x && snakeY == food.y) {
-        score++;
-        food = { x: Math.floor(Math.random() * 19 + 1) * box, y: Math.floor(Math.random() * 19 + 1) * box };
-        document.getElementById('scoreVal').innerText = 'Score: ' + score;
-    } else {
-        snake.pop();
-    }
+  let newHead = { x: snakeX, y: snakeY };
 
-    let newHead = { x: snakeX, y: snakeY };
+  if (snakeX < 0 || snakeX >= canvas.width || snakeY < 0 || snakeY >= canvas.height || collision(newHead, snake)) {
+    clearInterval(game);
+    alert("Partie terminée. Score : " + score);
+    return;
+  }
 
-    if (snakeX < 0 || snakeX >= canvas.width || snakeY < 0 || snakeY >= canvas.height || collision(newHead, snake)) {
-        clearInterval(game);
-        alert('Game Over. Score: ' + score);
-        document.getElementById('restartButton').style.display = 'block';
-    }
+  snake.unshift(newHead);
 
-    snake.unshift(newHead);
+  ctx.fillStyle = "white";
+  ctx.font = "20px Arial";
+  ctx.fillText("Score: " + score, box, 1.6 * box);
 }
 
-let game = setInterval(draw, 100);
-
-document.getElementById('restartButton').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('replayButton').addEventListener('click', () => {
+    clearInterval(game);
     snake = [{ x: 9 * box, y: 10 * box }];
     score = 0;
-    document.getElementById('scoreVal').innerText = 'Score: ' + score;
-    d = undefined;
-    food = { x: Math.floor(Math.random() * 19 + 1) * box, y: Math.floor(Math.random() * 19 + 1) * box };
-    this.style.display = 'none';
-    clearInterval(game);
+    d = null;
+    createFood();
     game = setInterval(draw, 100);
+  });
+
+  let imagesLoaded = 0;
+
+  appleImg.onload = snakeImg.onload = () => {
+    imagesLoaded++;
+    if (imagesLoaded === 2) {
+      createFood();
+      game = setInterval(draw, 100);
+    }
+  };
 });
+
+document.addEventListener("keydown", direction);
+function direction(event) {
+  if (event.keyCode == 37 && d != "RIGHT") d = "LEFT";
+  else if (event.keyCode == 38 && d != "DOWN") d = "UP";
+  else if (event.keyCode == 39 && d != "LEFT") d = "RIGHT";
+  else if (event.keyCode == 40 && d != "UP") d = "DOWN";
+}
